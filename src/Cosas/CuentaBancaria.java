04dab8cb958979fa4adf288cbc934be9bd1cca5f
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-04-29 22:45:54 -0500

    CuentaBancaria
    done

--- src/Cosas/CuentaBancaria.java
+++ src/Cosas/CuentaBancaria.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Cosas;
 
-/**
- *
- * @author Santi
- */
 public class CuentaBancaria {
-    
+    private int saldoActual = 100;
+    public int getSaldoActual(){
+        return saldoActual;
+    }
+    public void retirarSaldo(int num){
+        saldoActual -= num;
+    }
 }
