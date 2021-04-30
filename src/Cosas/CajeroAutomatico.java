88b9a641afce243a31c08ed9043b20f4672c7301
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-04-29 22:35:32 -0500

    Initial commit

---
+++ src/Cosas/CajeroAutomatico.java
@@ -0,0 +1,11 @@
+
+package Cosas;
+
+public class CajeroAutomatico implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
