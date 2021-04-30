1957f602ea57e2b65e957e03dd37e390fffee6a5
SamuelEsSantiago <76755769+SamuelEsSantiago@users.noreply.github.com>
2021-04-30 08:15:26 -0500

    Done?
    parece que ya

--- src/Cosas/CajeroAutomatico.java
+++ src/Cosas/CajeroAutomatico.java
@@ -8,14 +8,17 @@ public class CajeroAutomatico implements Runnable{
         for(int i=0;i<5;i++){
             try{
                 retirarDinero(10);
+                if(miCuenta.getSaldoActual()<0){
+                    System.err.println("Saldo negtivo!!");
+                }
                 Thread.sleep(500);
             }catch(InterruptedException ex){
-                
+                ex.printStackTrace();
             }
             
         }
     }
-    private void retirarDinero(int retirar){
+    private synchronized void retirarDinero(int retirar){
         if(retirar < miCuenta.getSaldoActual()){
             System.out.println("Saldo actual=" + miCuenta.getSaldoActual());
             System.out.println("Usuario:" + Thread.currentThread().getName()+ " Retiro: $" +retirar);
--- src/sincronizando/Sincronizando.java
+++ src/sincronizando/Sincronizando.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sincronizando;
 
-/**
- *
- * @author Santi
- */
+import Cosas.CajeroAutomatico;
+
 public class Sincronizando {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CajeroAutomatico cajero = new CajeroAutomatico();
+        Thread pepito = new Thread(cajero,"Pepito");
+        Thread juanito = new Thread(cajero,"Juanito");
+        pepito.start();
+        juanito.start();
     }
     
 }
